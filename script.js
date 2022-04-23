119efd8a3f7c3a05ee4cc1bff2b55f6625fa4944
Akriti Sinha <56969972+akriti-2001@users.noreply.github.com>
2022-04-23 17:20:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    // Get  input values
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+
+    // Validation
+    if (principal <= 0) {
+        alert("Enter a positive number");
+
+        document.getElementById("principal").focus();
+        document.getElementById("principal").click();
+    }
+
+    // Computations
+    interest = principal * years * rate / 100;
+    endYear = new Date().getFullYear() + parseInt(years);
+
+    // Display Result
+    el = document.getElementById("result");
+    el.innerHTML = "If you deposit <span class='highlight'><mark>" + principal + "</mark></span><br/>";
+    el.innerHTML += "at an interest rate of <span class='highlight'><mark>" + rate + "%." + "</mark></span><br/>";
+    el.innerHTML += "You will recieve an amount of <span class='highlight'><mark>" + interest + "</mark></span>,<br/>";
+    el.innerHTML += "in the year <span class='highlight'><mark>" + endYear + "</mark></span>.<br/>";
 }
-        
+
+function rateUpdate() {
+    rate = document.getElementById("rate").value;
+    document.getElementById("rate-update").innerHTML = rate + "%";
+}
